b73bb0ec9bf2ae08031e2a04a22758f187e899e5
Анастасия Бойко <anastasia151000@rambler.ru>
2021-04-25 23:42:33 +0300

    Страницы

--- Script.js
+++ Script.js
@@ -1,1 +1,84 @@
-// JavaScript source code
+var arr = [];
+
+function ButNext(name) {
+
+    var element = document.querySelector('input[name="AT"]:checked').value;
+    if (element) {
+        document.location = "Page2.html";
+    }
+    else { alert("Заполните поля!"); }
+   
+
+}
+
+
+function ButNext2(name) {
+
+    var element2 = document.querySelector('input[name="AT"]:checked').value;
+    if (element2) {
+        document.location = "Page3.html";
+    }
+    else { alert("Заполните поля!"); }
+   
+
+}
+function ButNext3(name) {
+
+    var element3 = document.querySelector('input[name="AT"]:checked').value;
+    if (element3) {
+
+
+    }
+    else { alert("Заполните поля!"); }
+    document.location = "Page4.html";
+
+}
+function ButNext4(name) {
+
+    var element4 = document.querySelector('input[name="AT"]:checked').value;
+    if (element4) {
+
+
+    }
+    else { alert("Заполните поля!"); }
+    document.location = "Page5.html";
+
+}
+
+function ButNext5(name) {
+
+    var element5 = document.querySelector('input[name="AT"]:checked').value;
+    if (element5) {
+
+
+    }
+    else { alert("Заполните поля!"); }
+    document.location = "Page6.html";
+
+}
+
+function ButNext6(name) {
+
+    var element6 = document.querySelector('input[name="AT"]:checked').value;
+    if (element6) {
+
+
+    }
+    document.location = "Page7.html";
+
+}
+
+function ButNext7(name) {
+
+    var element7 = document.querySelector('input[name="AT"]:checked').value;
+
+    if (element7) {
+
+    }
+
+
+    /*element2, element3, element4, element5, element6, element7);*/
+    
+}
+
+
